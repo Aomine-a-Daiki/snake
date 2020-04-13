c256c9d3c10f123be00baa230375be5eae10d441
Aomine-a-Daiki <batir9696@gmail.com>
2020-04-13 21:20:16 +0600

    Lesson11. Полноценное передвижение и управление змейкой

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -26,29 +26,21 @@ namespace Snake
             Point p1 = new Point(4, 5, '*');
             Snake snake = new Snake(p1, 4, Direction.RIGHT);
             snake.Draw();
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-
-
-            Console.ReadKey();
+            
+            while(true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    snake.HandleKey(key.Key);
+                }
+                Thread.Sleep(100);
+                snake.Move();
+            }
+            
+            //Console.ReadKey();
         }
+
 
         static void Draw(int x, int y, char sym)
             {
